328f6cfb6be493369431d3279a4f125ded9764f6
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2022-02-28 17:28:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
+global.owner = ['6282268562601']
 global.packname = 'AS-STYLE'
 global.author = 'MD'
 global.sessionName = 'mek'
