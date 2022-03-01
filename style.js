2184251507995c615c0c6e55416b19d82d97d158
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2022-03-01 20:13:36 +0700

    Add files via upload

--- style.js
+++ style.js
@@ -13,6 +13,7 @@ const { exec, spawn, execSync } = require("child_process")
 const axios = require('axios')
 const path = require('path')
 const os = require('os')
+const fetch = require('node-fetch')
 const moment = require('moment-timezone')
 const { JSDOM } = require('jsdom')
 const speed = require('performance-now')
@@ -683,34 +684,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]} untuk ketik terima/tolak`
                 if (!text) throw 'Text ?'
                 await style.groupUpdateDescription(m.chat, text).then((res) => m.reply(mess.success)).catch((err) => m.reply(jsonformat(err)))
             }
-            break
-            case 'upswimg':
-                    if (!isCreator) throw mess.owner
-                    var teksyy = body.slice(9)
-                    m.reply('Sedang Proses Pengiriman!')
-                    gwambar = JSON.parse(JSON.stringify(m).replace('quoted','m')).message.extendedTextMessage.contextInfo
-					gambar = await style.downloadAndSaveMediaMessage(gwambar)
-                    buff2 = fs.readFileSync(gambar)
-                    style.sendMessage('status@broadcast', buff2, MessageType.image, {quoted: m, caption: `${teksyy}`})
-                    m.reply('Sukses Upload Gambar Ke Status!')
-                        break
-                        case 'upswvid':
-                        if (!isCreator) throw mess.owner
-                    var teksyy = body.slice(9)
-                    m.reply('Sedang Mengupload!')
-                    pideo = JSON.parse(JSON.stringify(m).replace('quoted','m')).message.extendedTextMessage.contextInfo
-					pisdo = await style.downloadAndSaveMediaMessage(pideo)
-                    buff5 = fs.readFileSync(pisdo)
-                    style.sendMessage('status@broadcast', buff5, MessageType.video, {quoted: m, caption: `${teksyy}`})
-                    m.reply('Sukses Upload Video Ke Status!')
-                        break
-                        case 'upsw':
-                        if (!isCreator) throw mess.owner
-                     teksyy = body.slice(6) 
-                    m.reply('Sedang Mengupload!')
-                    style.sendMessage('status@broadcast', MessageType.text) 
-                    m.reply('Sukses Upload Ke Status!')
-                        break
+            break            
           case 'setppbot': {
                 if (!isCreator) throw mess.owner
                 if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
