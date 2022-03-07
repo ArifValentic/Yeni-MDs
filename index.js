de82fdee58d81e3a831fc60c516c8f8dec1d1f60
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2022-03-07 12:46:06 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const yargs = require('yargs')
 const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/myfunc')
+var low
+try {
+  low = require('lowdb')
+} catch (e) {
+  low = require('./lib/lowdb')
+}
+const { Low, JSONFile } = low
 
 global.api = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
